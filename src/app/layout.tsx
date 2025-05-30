04cd57f34a8c2f602c4aa6a5e5d4ee116d0f60dc
bgocumlu <bgocumlu@users.noreply.github.com>
2025-05-30 13:45:58 +0300

    add mobile web app meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Apple PWA Meta Tags */}
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Ucucu" />
